4581cbb91272ae65f7032547fe862c262d579eb8
EnriqueCeballosMartinez <ceballos_kik96@hotmail.com>
2019-11-23 22:55:25 -0600

    Se agregaron pantallas
    Se agregaron pantallas para el chat

---
+++ app/src/main/java/com/example/holapp/menu_chats.kt
@@ -0,0 +1,12 @@
+package com.example.holapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class menu_chats : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu_chats)
+    }
+}
